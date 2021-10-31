b746bc91b2f837707a2f48329d506860a5610e5e
Ed Ilyin <ed.il@live.com>
2021-10-31 11:28:11 +0200

    PowderBlue Favour

--- src/Cleverage.Bot/Program.fs
+++ src/Cleverage.Bot/Program.fs
@@ -1,20 +1,32 @@
 open Funogram.Telegram.Bot
 open Funogram.Telegram.Api
+open Funogram.Api
 
-let accessToken = "мой токен"
+let accessToken = ""
+let config = { defaultConfig with Token = accessToken }
 
 let onStart (context: UpdateContext) = printfn "start %+A" context
 let onHelp (context: UpdateContext) = printfn "help %+A" context
 
-let onUpdate (context: UpdateContext) =
-    processCommands context [
+let onSay arg (ctx: UpdateContext) =
+    // let chatId = Option.map (fun m -> m.Chat.Id) ctx.Update.Message
+    match ctx.Update.Message with
+        | None -> ()
+        | Some msg ->
+            Some  msg.MessageId
+            |> sendMessageReply msg.Chat.Id arg
+            |> api config
+            |> Async.RunSynchronously
+            |> ignore
+
+let onUpdate (ctx: UpdateContext) =
+    processCommands ctx [
         cmd "/start" onStart
         cmd "/help" onHelp
-        cmd "/msg" (fun _ -> sendMessage
-    ] |> printfn "onUpdate %+A %b" context.Update.Message
+        cmdScan "/say %s" onSay
+    ] |> printfn "onUpdate %+A %b" ctx.Update.Message
 
 [<EntryPoint>]
 let main argv =
-    startBot { defaultConfig with Token = accessToken } onUpdate None
-    |> Async.RunSynchronously
+    startBot config onUpdate None |> Async.RunSynchronously
     0 // return an integer exit code
