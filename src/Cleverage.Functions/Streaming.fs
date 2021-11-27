6ca2fe3a876ddb3711beea77171a0270af685710
Ed Ilyin <ed.il@live.com>
2021-11-27 17:59:27 +0200

    last

--- src/Cleverage.Functions/Streaming.fs
+++ src/Cleverage.Functions/Streaming.fs
@@ -9,12 +9,13 @@ open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.Azure.WebJobs.Extensions.SignalRService
 open Newtonsoft.Json
+open Microsoft.Extensions.Logging
 
 let private httpClient = new HttpClient()
 
 [<FunctionName("index")>]
 let Index
-    ([<HttpTrigger(AuthorizationLevel.Anonymous)>] req: HttpRequest)
+    ([<HttpTrigger(AuthorizationLevel.Function)>] req: HttpRequest)
     (ctx: ExecutionContext) =
     let path = Path.Combine(ctx.FunctionAppDirectory, "content", "index.html")
     ContentResult (Content = File.ReadAllText(path), ContentType = "text/html")
@@ -29,10 +30,11 @@ let Negotiate
 let Broadcast
     ([<HttpTrigger(AuthorizationLevel.Function)>] req: HttpRequestMessage)
     ([<SignalR(HubName = "CLeveRAge")>]
-        signalRMessages: IAsyncCollector<SignalRMessage>) = task {
+        signalRMessages: IAsyncCollector<SignalRMessage>)
+    (log: ILogger) = task {
 
     let! content = req.Content.ReadAsStringAsync ()
-
+    log.LogInformation $"😈: {content}"
     return!
         signalRMessages.AddAsync (
             SignalRMessage (Target = "newMessage", Arguments = [| content |])
