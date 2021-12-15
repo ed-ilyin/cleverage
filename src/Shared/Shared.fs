5d48a2e1499c2bece0b60ba05d96dc562955dec7
Ed Ilyin <ed.il@live.com>
2021-12-15 21:29:46 +0200

    пофиксил декодер

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -7,25 +7,28 @@ type Update =
         Chat: string
         Text: string }
     static member Decoder : Decoder<Update> =
-        Decode.object (fun get -> {
+        let decodeMessage = Decode.object (fun get -> {
+                MessageId =
+                    get.Required.Field "message_id" Decode.uint64
 
-            MessageId =
-                get.Required.Field "message_id" Decode.uint64
+                From = get.Required.At [ "from"; "first_name" ] Decode.string
 
-            From = get.Required.At [ "from"; "first_name" ] Decode.string
+                Chat =
+                    get.Optional.At [ "chat"; "title" ] Decode.string
+                    |> Option.orElse
+                    <| get.Optional.At [ "chat"; "first_name" ] Decode.string
+                    |> Option.defaultValue ""
 
-            Chat =
-                get.Optional.At [ "chat"; "title" ] Decode.string
-                |> Option.orElse
-                <| get.Optional.At [ "chat"; "first_name" ] Decode.string
-                |> Option.defaultValue ""
+                Text =
+                    get.Optional.Field "text" Decode.string
+                    |> Option.defaultValue ""
 
-            Text =
-                get.Optional.Field "text" Decode.string
-                |> Option.defaultValue ""
+            })
+        Decode.oneOf [
+            decodeMessage
+            decodeMessage |> Decode.field "message"
+        ]
 
-        })
-        |> Decode.field "message"
     static member Encoder update =
         Encode.Auto.generateEncoderCached<Update>
             (PascalCase, Extra.withUInt64 Extra.empty)
