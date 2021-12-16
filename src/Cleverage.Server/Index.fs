61a3e61ae797e446815664f622f0a99b6ae90377
Alex Zaslavskis <sahsariga111@gmail.com>
2021-12-16 22:01:06 +0200

    some fixes

--- src/Cleverage.Server/Index.fs
+++ src/Cleverage.Server/Index.fs
@@ -9,7 +9,7 @@ let page = doctypeHtml [] [
     head [] [
         meta [attr.charset "UTF-8"]
         meta [attr.name "viewport"; attr.content "width=device-width, initial-scale=1.0"]
-        title [] [text "Bolero Application"]
+        title [] [text "CLeveRAge | СОобРАЖатор"]
         ``base`` [attr.href "/"]
         link [attr.rel "stylesheet"; attr.href "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"]
         link [attr.rel "stylesheet"; attr.href "css/index.css"]
@@ -18,8 +18,8 @@ let page = doctypeHtml [] [
         nav [attr.classes ["navbar"; "is-dark"]; "role" => "navigation"; attr.aria "label" "main navigation"] [
             div [attr.classes ["navbar-brand"]] [
                 a [attr.classes ["navbar-item"; "has-text-weight-bold"; "is-size-5"]; attr.href "https://fsbolero.io"] [
-                    img [attr.style "height:40px"; attr.src "https://github.com/fsbolero/website/raw/master/src/Website/img/wasm-fsharp.png"]
-                    text "  Bolero"
+                    img [attr.style "height:40px"; attr.src "./data/logo.png"]
+                    text "CLeveRAge | СОобРАЖатор"
                 ]
             ]
         ]
