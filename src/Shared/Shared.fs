ed3c9e95deea88565776cad2627e6bf8e46ca7d5
Ed Ilyin <ed.il@live.com>
2021-12-16 07:21:18 +0200

    добавил SessionName

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -7,25 +7,27 @@ type Update =
         Chat: string
         Text: string }
     static member Decoder : Decoder<Update> =
-        let decodeMessage = Decode.object (fun get -> {
-                MessageId =
-                    get.Required.Field "message_id" Decode.uint64
-
-                From = get.Required.At [ "from"; "first_name" ] Decode.string
-
-                Chat =
-                    get.Optional.At [ "chat"; "title" ] Decode.string
-                    |> Option.orElse
-                    <| get.Optional.At [ "chat"; "first_name" ] Decode.string
-                    |> Option.defaultValue ""
-
+        let decodeMessage = Decode.object (fun get ->
+            let from = get.Required.At [ "from"; "first_name" ] Decode.string
+            let chat =
+                get.Optional.At [ "chat"; "title" ] Decode.string
+                |> Option.orElse
+                <| get.Optional.At [ "chat"; "first_name" ] Decode.string
+                |> Option.defaultValue ""
+            {   MessageId = get.Required.Field "message_id" Decode.uint64
+                From = if from = chat then "" else from
+                Chat = chat
                 Text =
                     get.Optional.Field "text" Decode.string
                     |> Option.defaultValue ""
-
-            })
+            }
+        )
         Decode.oneOf [
-            decodeMessage
+            Decode.map2
+            <| fun sessionName message ->
+                { message with Chat = sessionName; From = "" }
+            <| Decode.field "SessionName" Decode.string
+            <| Decode.field "Message" decodeMessage
             decodeMessage |> Decode.field "message"
         ]
 
