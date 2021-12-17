2163c3d06e3d5b8a7853f315d7933686d6da7de5
Alex Zaslavskis <sahsariga111@gmail.com>
2021-12-17 16:30:04 +0200

    awesome icons patch
    1) added correct titles
    2) added the icon in png on top page and in nav bar
    3) inverted color using css
    
    Co-Authored-By: Ed Ilyin <5064271+ed-ilyin@users.noreply.github.com>

--- src/Cleverage.Server/Index.fs
+++ src/Cleverage.Server/Index.fs
@@ -13,12 +13,14 @@ let page = doctypeHtml [] [
         ``base`` [attr.href "/"]
         link [attr.rel "stylesheet"; attr.href "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"]
         link [attr.rel "stylesheet"; attr.href "css/index.css"]
+        link [attr.rel "icon"; attr.classes ["icon_top"]; attr.href "favicon.ico" ]
+
     ]
     body [] [
         nav [attr.classes ["navbar"; "is-dark"]; "role" => "navigation"; attr.aria "label" "main navigation"] [
             div [attr.classes ["navbar-brand"]] [
                 a [attr.classes ["navbar-item"; "has-text-weight-bold"; "is-size-5"]; attr.href "https://fsbolero.io"] [
-                    img [attr.style "height:40px"; attr.src "https://raw.githubusercontent.com/alex5250/cleverage/master/src/Cleverage.Server/logo.jpg"]
+                    img [attr.classes ["game_icon"]; attr.src "logo.png"]
                     text "CLeveRAge | СОобРАЖатор"
                 ]
             ]
